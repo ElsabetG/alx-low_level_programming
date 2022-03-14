0930db7a530427add10ad8be38f2f07b593ea017
ElsabetG <elsabetgabre@gmail.com>
2022-03-14 12:20:48 -0700

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 /*
  * File: 1-last_digit.c
  * Auth: Elsabet Gebre
+ */
 
 #include <stdlib.h>
 #include <time.h>
@@ -8,6 +9,7 @@
 
 /**
  * main - Prints the last digit of a randomly generated number
+ *
  * Return: Always 0.
  */
 int main(void)
