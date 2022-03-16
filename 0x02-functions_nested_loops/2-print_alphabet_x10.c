d1ce7c862b1f1e4049bebd6b13b71ff182d7aeb6
ElsabetG <elsabetgabre@gmail.com>
2022-03-16 13:50:38 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+i#include "main.h"
+/**
+ * print_alphabet_x10 - print a-z X10
+ *
+ * Description: print the alphabet in lower case
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+
+		ch = 'a';
+
+		_putchar('\n');
+	}
+}
