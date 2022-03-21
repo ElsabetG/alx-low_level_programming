6edbc7d27a1866c7725c08437d0b05adf2821f9e
ElsabetG <elsabetgabre@gmail.com>
2022-03-21 11:52:05 -0700

    new

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,15 +6,14 @@
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
  */
-char *_strncat(char *dest, char *src, int n)
+void swap_int(int *a, int *b)
+
 {
-	int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+	int tmp = *a;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	*a = *b;
 
-	return (dest);
+	*b = tmp;
+
 }
