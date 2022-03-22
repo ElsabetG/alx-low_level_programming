c17b8e36d08a9e70167788d5eee50ec118fd8c3b
ElsabetG <elsabetgabre@gmail.com>
2022-03-22 12:53:44 -0700

    new

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,38 +1,18 @@
 #include "main.h"
 /**
- * puts_half - puts half of a string
- * @str: string
- * Return:void
+ * _strcpy - Copy paste string
+ * @dest: destination
+ * @src: source
+ * Return: dest
  */
-void puts_half(char *str)
+char *_strcpy(char *dest, char *src)
 {
-	int len = _strlen(str);
-
-	if (len % 2 == 0)
-		len = len / 2;
-
-	else
-		len = (len + 1) / 2;
-
-	while (str[len] != '\0')
+	int inc = 0;
+	while (*(src + inc) != '\0')
 	{
-		_putchar(str[len]);
-		len++;
-	}
-	_putchar('\n');
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string
- * Return: returns lenght;
- */
-int _strlen(char *s)
-{
-	int count, inc;
-	inc = 0;
-	for (count = 0; s[count] != '\0'; count++)
+		*(dest + inc) = *(src + inc);
 		inc++;
-
-	return (inc);
+	}
+	*(dest + inc) = '\0';
+	return (dest);
 }
