deb20f0c4c90294f6be361576eb54f344a1c6c36
ElsabetG <elsabetgabre@gmail.com>
2022-03-22 06:11:33 -0700

    last

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings using at most
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
+ * swap_int - swaps the values of a and b
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return; 0
  */
+
 void swap_int(int *a, int *b)
-
 {
-
-	int tmp = *a;
-
-	*a = *b;
-
-	*b = tmp;
-
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,18 +8,11 @@
  *
  * Return: A pointer to the resulting string dest.
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strlen(char *s)
 {
-	int index = 0, src_len = 0;
+	int length = 0;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
+	while (s[length])
+		length++;
+	return (length);
 }
