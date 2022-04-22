741abfbabd6697591b425300da4bee3cb5ee3ec8
ElsabetG <elsabetgabre@gmail.com>
2022-04-22 14:17:51 -0700

    commiting new files

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,17 +2,17 @@
 
 /**
  * list_len - Finds the number of elements in
- *  a linked list_t list.
- *  @h: The linked list_t list.
- *  @elements: a counter var for size_t (nodes)
+ * a linked list_t list.
+ * @h: The linked list_t list.
+ * @elements: a counter var for size_t (nodes)
  *
- *  Return: The number of elements in h.
+ * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
 
-	while(h)
+	while (h)
 	{
 		elements++;
 		h = h->next;
